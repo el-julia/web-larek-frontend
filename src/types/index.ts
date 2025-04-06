436122d71d1e9074fc1daf79bc2cef8b70981c2a
Julia Shkolina <jsk108@yandex.ru>
2025-04-06 21:12:54 +0400

    автоформат

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,115 +1,106 @@
-
-
 export interface IProductItems {
-    getProductList: () => Promise<ProductList>;
-    getProduct: (id: string) => Promise<Product>;
-    placeOrder: (order: Order) => Promise <OrderResult>;
+	getProductList: () => Promise<ProductList>;
+	getProduct: (id: string) => Promise<Product>;
+	placeOrder: (order: Order) => Promise<OrderResult>;
 }
-
 
 // описание типа категории товара
 export type ProductCategory = {
-    category: 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
-}
-
+	category: 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
+};
 
 //описание интерфейса карточки товара
 export interface Product {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number;
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number;
 }
+
 export interface ProductList {
-    items: Product[];
+	items: Product[];
 }
 
 export interface Cart {
-    title: string;
-    price: number;
+	title: string;
+	price: number;
 }
 
 export interface OrderAddresses {
-    payment: string;
-    address: string;
+	payment: string;
+	address: string;
 }
 
 export interface OrderContacts {
-    email: string;
-    phone: number;
+	email: string;
+	phone: number;
 }
 
-export interface Order extends OrderAddresses, OrderContacts{}
+export interface Order extends OrderAddresses, OrderContacts {}
 
 export interface OrderResult {
-    id: string;
-    total: number;
+	id: string;
+	total: number;
 }
-
 
 export type FormErrors = Partial<Record<keyof Order, string>>;
 
-
-
-
-
 export type ApiListResponce<Type> = {
-    total: number;
-    items: Type[];
-}
+	total: number;
+	items: Type[];
+};
 
 export enum EnumApiMethods {
-    POST = 'POST',
-    PUT = 'PUT', //надо?
-    DELETE = 'DELETE', //надо?
-    GET = 'GET',
+	POST = 'POST',
+	PUT = 'PUT', //надо?
+	DELETE = 'DELETE', //надо?
+	GET = 'GET',
 }
 
 export type ErrorState = {
-    error: string;
-}
-
-
+	error: string;
+};
 
 export type EventData = object;
 export type EventHandler = (args: EventData) => void;
 export type EventsMap = Map<string, Set<EventHandler>>;
 
-
-
 export interface IView<T, S = object> {
-    // отображение для заданного типа данных
-    element: HTMLElement; // корневой элемент
-    copy(settings?: S): IView<T>; // копирующий конструктор
-    render(data?: Partial<T>): HTMLElement; // метод рендера
+	// отображение для заданного типа данных
+	element: HTMLElement; // корневой элемент
+	copy(settings?: S): IView<T>; // копирующий конструктор
+	render(data?: Partial<T>): HTMLElement; // метод рендера
 }
 
 export interface IViewConstructor<T, S> {
-    // конструктор отображения
-    // получает на вход клонированный шаблон
-    // или существующий элемент,
-    // а также настройки для отображения
-    new(root: HTMLElement, settings: S): IView<T>;
+	// конструктор отображения
+	// получает на вход клонированный шаблон
+	// или существующий элемент,
+	// а также настройки для отображения
+	new (root: HTMLElement, settings: S): IView<T>;
 }
 
 // Чтобы события настраивались единообразно, пропишем их здесь
 
 // Настройки для кликабельного отображения (кнопки, карточки...)
 export type IClickableEvent<T> = { event: MouseEvent; item?: T };
+
 export interface IClickable<T> {
-    onClick: (args: IClickableEvent<T>) => void;
+	onClick: (args: IClickableEvent<T>) => void;
 }
 
 // Настройки для изменяемого отображения (формы, переключатели...)
 export type IChangeableEvent<T> = { event: Event; value?: T };
+
 export interface IChangeable<T> {
-    onChange: (args: IChangeableEvent<T>) => void;
+	onChange: (args: IChangeableEvent<T>) => void;
 }
 
 // Настройки для выбираемого отображения (списки, таблицы...)
 export type ISelectableEvent<T> = { event: Event; value?: T };
+
 export interface ISelectable<T> {
-    onSelect: (args: ISelectableEvent<T>) => void;
+	onSelect: (args: ISelectableEvent<T>) => void;
 }
