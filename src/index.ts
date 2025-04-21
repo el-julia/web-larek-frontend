8891bdbabfc874712a6c8be7bed0280fa62263ae
Julia Shkolina <jsk108@yandex.ru>
2025-04-21 16:48:15 +0400

    подписка на изменения моделей

--- src/index.ts
+++ src/index.ts
@@ -46,10 +46,10 @@ const page = new Page(document.body, {
 });
 
 // модель данных приложения
-const productsModel = new Catalog(events);
-const basketModel = new BasketModel(events);
-const orderModel = new OrderModel(events);
-const contactModel = new ContactsModel(events);
+const catalogModel = new Catalog(events, CatalogEvents.CHANGED);
+const basketModel = new BasketModel(events, BasketEvents.CHANGED);
+const orderModel = new OrderModel(events, CheckoutEvent.ORDER_CHANGED);
+const contactModel = new ContactsModel(events, CheckoutEvent.CONTACTS_CHANGED);
 
 const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
 
@@ -107,14 +107,14 @@ let productCardPreview: CardPreview;
 api
 	.getProductList()
 	.then((products) => {
-		productsModel.setProducts(products);
+		catalogModel.setProducts(products);
 	})
 	.catch((error) => {
 		console.error(error);
 	});
 
 // рендерим список товаров на странице
-events.on(CatalogEvents.CHANGED, (products: Product[]) => {
+catalogModel.subscribe(products => {
 	page.catalog = products.map((product) => {
 		const card = new CardCatalog(cloneTemplate(cardCatalogTemplate), {
 			onClick: () => events.emit(ModalEvents.PRODUCT_PREVIEW, product),
@@ -170,17 +170,17 @@ events.on(BasketEvents.CLEAR, () => {
 	basketModel.clear();
 });
 
-events.on(BasketEvents.CHANGED, (products: Product[]) => {
+basketModel.subscribe(products => {
 	page.counter = products.length;
 });
 
-events.on(BasketEvents.CHANGED, (products: Product[]) => {
+basketModel.subscribe(products => {
 	if (productCardPreview !== undefined) {
 		productCardPreview.basketProducts = products;
 	}
 });
 
-events.on(BasketEvents.CHANGED, (products: Product[]) => {
+basketModel.subscribe(products => {
 	const cardBasketItems = products.map((product, index) => {
 		const cardBasket = new CardBasket(cloneTemplate(cardBasketTemplate), {
 			onCardButtonDeleteClick: () => events.emit(BasketEvents.REMOVE, product),
@@ -197,7 +197,7 @@ events.on(BasketEvents.CHANGED, (products: Product[]) => {
 		items: cardBasketItems,
 		total: products.reduce((total, item) => total + item.price, 0),
 	});
-});
+})
 
 //оформление заказа по клику оформить в корзине
 
@@ -231,19 +231,17 @@ events.on(CheckoutEvent.ADDRESS_INPUT, (data: Pick<IOrderChange, 'address'>) => 
 	orderModel.setAddress(data.address);
 });
 
-events.on(CheckoutEvent.ORDER_CHANGED, (data: IOrderChange) => {
+orderModel.subscribe(data => {
 	order.payment = data.payment;
 	order.address = data.address;
 	order.valid = data.valid;
-});
+})
 
-events.on(CheckoutEvent.CONTACTS_CHANGED, (data: IContactsChange) => {
+contactModel.subscribe(data => {
 	contacts.email = data.email;
 	contacts.phone = data.phone;
 	contacts.valid = data.valid;
-});
-
-//откуда это data приходит?
+})
 
 events.on(CheckoutEvent.EMAIL_INPUT, (data: Pick<IContactsChange, 'email'>) => {
 	contactModel.setEmail(data.email);
@@ -253,11 +251,9 @@ events.on(CheckoutEvent.PHONE_INPUT, (data: Pick<IContactsChange, 'phone'>) => {
 	contactModel.setPhone(data.phone);
 });
 
-//а как понять что нужно email передать
-//и откуда придет
 
 // для разработки
-events.on(CatalogEvents.CHANGED, (products: Product[]) => {
+catalogModel.subscribe(products => {
 	events.emit(BasketEvents.ADD, products.pop());
 	events.emit(ModalEvents.CONTACTS);
-});
+})
